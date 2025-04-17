a8ee3bd3faf7b30c533a820c28dac5b4256b18b3
iznoelc <izzybellanoel@gmail.com>
2025-04-17 10:27:21 -0400

    Updated player.cpp to add health & damage management and movement

--- Assignment_03/src/Player.cpp
+++ Assignment_03/src/Player.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "Player.h"
 
-Player::Player(std::string texturePath)
+Player::Player(std::string texturePath, float speed)
 {
 
 	// Load texture from file
@@ -15,25 +15,76 @@ Player::Player(std::string texturePath)
 	sprite.setTexture(texture);
 	
 	// set texture rect based on walking sprite (Cat 1) (sprite sheet for walking animation is 6 frames, 38 x 25)
-	sprite.setTextureRect(sf::IntRect(0, 0, 38, 25));
+	sprite.setTextureRect(sf::IntRect(0, 0, 32, 20));
 
 	// set position of the player
 	sprite.setPosition(400,300);
 
 	// set player center to center of sprite
-	sprite.setOrigin(19, 12);
+	sprite.setOrigin(16, 10);
 
 	sprite.setScale(2,2); 
+
+	// set player speed
+	this->speed = speed;
+	
+	// define a hitbox for the player
+	// hitbox = Hitbox({22.f, 20.f}, sf::Color(0, 255, 0, 100)); // light green color for player
+}
+
+// get the player's potion
+sf::Vector2f Player::getPosition()
+{
+	return sprite.getPosition();
+}
+
+// get player's global bounds
+sf::FloatRect Player::getGlobalBounds()
+{
+	return sprite.getGlobalBounds();
 }
 
 void Player::draw(sf::RenderWindow& window)
 {
-	// draw the player
+	// draw the player & hitbox
 	window.draw(sprite);
+	// hitbox.draw(window);
 }
 
+// direction player is facing; used to flip sprite if needed
 int direction = 1;
-// no movement yet so direction stagnant
+
+void Player::update(float dt)
+{
+	handleInput(dt);
+	timeSinceLastHit += dt;
+
+	// set player hitbox with an offet (NOT sure about this number, may need to be adjusted)
+	//hitbox.setPosition({sprite.getPosition().x, sprite.getPosition().y + 10});
+}
+
+void Player::handleInput(float dt)
+{
+	sf::Vector2f movement(0,0);
+
+	// movement A->left, D->right
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) // moving left
+	{
+		direction = -1;
+		movement.x -= speed;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) // moving right
+	{
+		direction = 1;
+		movement.x += speed;
+	}
+
+	// actually move the player
+	sprite.move(movement * speed * dt);
+
+	// handle the animation
+	handleAnimation(direction, dt);
+}
 
 void Player::handleAnimation(int direction, float dt)
 {
@@ -48,8 +99,11 @@ void Player::handleAnimation(int direction, float dt)
 		}
 
 		// update texture rect to show current frame
-		sprite.setTectureRect(sf::IntRect(currentFrame * 38, 0, 38, 25));
-		
+		sprite.setTextureRect(sf::IntRect(currentFrame * 38, 0, 38, 25));
+			
+		// flip sprite
+		sprite.setScale(2 * direction, 2); 
+
 		// then, update current frame
 		currentFrame++; 
 		
@@ -57,4 +111,27 @@ void Player::handleAnimation(int direction, float dt)
 		timeSinceLastFrame = 0;
 
 	}	
+}
+
+// get player health
+int Player::getHealth() const
+{
+	return health;
+}
+
+// make player take damage
+void Player::takeDamage(int amount)
+{
+	if (canTakeDamage())
+	{
+		health -= amount;
+		std::cout << "Player took damage!" << std::endl;
+		timeSinceLastHit = 0.0f;
+	}
+}
+
+// check if player can take damage
+bool Player::canTakeDamage() const
+{
+	return timeSinceLastHit >= damageCooldown;
 }
