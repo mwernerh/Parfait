466365c578756684502a26317953a7c6aeae40e8
iznoelc <izzybellanoel@gmail.com>
2025-05-14 20:22:41 -0400

    Add game title text, change window title

--- Project/src/Gamestates/TitleScreenGameState.cpp
+++ Project/src/Gamestates/TitleScreenGameState.cpp
@@ -12,9 +12,18 @@ TitleScreenGameState::TitleScreenGameState() : InstancedGameState(), startText("
     background.setTexture(TextureManager::GetTexture("bgs/s_ct/1.png"));
     background.setOrigin(0.f, 0.f);
     background.setPosition(-50.f, -150.f);
-	
+    
+    titleText.setFont(FontManager::GetFont("Outspace Fighter"));
+    titleText.setString("Homesick");
+    titleText.setCharacterSize(54);
+    titleText.setOrigin(titleText.getLocalBounds().width / 2.0f, titleText.getLocalBounds().height / 2.0f);
+    titleText.setPosition({1280.0f / 2.f, (720.0f / 2.0f) - (720.f / 3.0f)});
+    titleText.setFillColor(sf::Color::White);
+    titleText.setOutlineThickness(2.f);
+    titleText.setOutlineColor(sf::Color::Black);
+
     startText.setOrigin(startText.getLocalBounds().width / 2.0f, startText.getLocalBounds().height / 2.0f);
-    startText.setPosition({1280.0f / 2.f, (720.0f / 2.f) - (720.f / 4.f)});
+    startText.setPosition({1280.0f / 2.f, (720.0f / 2.f) - (720.f / 4.0f)});
     startText.setFillColor(sf::Color::White);
     startText.setOutlineThickness(2.f);
     startText.setOutlineColor(sf::Color::Black);
@@ -31,5 +40,6 @@ void TitleScreenGameState::Update() {
 void TitleScreenGameState::Draw(sf::RenderWindow& window) {
     window.draw(background);
     GetInstancedOperator<TitleScreenActorOperator>().Draw(window);
+    window.draw(titleText);
     window.draw(startText);
 }
